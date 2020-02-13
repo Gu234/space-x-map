93c31f47fd146d35dd7570a99367923219859949
Gu234 <guciofilipek@gmail.com>
2020-02-13 13:36:36 +0100

    working leaflet map
    Co-authored-by: Amadeusz <amadeusz.filipek@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import './App.css';
+
 import { createApolloFetch } from 'apollo-fetch';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+
+const position = [51.505, -0.09]
+const map =
+  <Map style={{ height: '200px' }} center={position} zoom={12}>
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+    />
+    <Marker position={position}>
+      <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+    </Marker>
+  </Map>
+
 
 const uri = 'https://api.spacex.land/graphql/';
 const apolloFetch = createApolloFetch({ uri });
@@ -31,7 +46,7 @@ apolloFetch({ query }) //all apolloFetch arguments are optional
 function App() {
   return (
     <div className="App">
-
+      {map}
     </div>
   );
 }
